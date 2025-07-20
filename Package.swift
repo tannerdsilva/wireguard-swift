6202f9abdff41c3e575a03a2232091cd05fdb1ca
tannerdsilva <t@tannersilva.com>
2025-07-20 15:07:06 -0500

    in the beginning...

---
+++ Package.swift
@@ -0,0 +1,52 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+	name: "wireguard-swift",
+	platforms:[
+		.macOS(.v15)
+	],
+	products: [
+		// Products define the executables and libraries a package produces, making them visible to other packages.
+		.executable(
+			name: "wg-test-tool",
+			targets: ["wg-test-tool"]),
+	],
+	dependencies: [
+		.package(url:"https://github.com/tannerdsilva/rawdog.git", "17.0.1"..<"18.0.0"),
+		.package(url:"https://github.com/apple/swift-log.git", "1.6.3"..<"2.0.0"),
+		.package(url:"https://github.com/apple/swift-nio.git", "2.84.0"..<"3.0.0"),
+		.package(url:"https://github.com/tannerdsilva/bedrock.git", "6.0.0"..<"7.0.0"),
+		.package(url:"https://github.com/apple/swift-argument-parser.git", "1.6.1"..<"2.0.0")
+	],
+	targets: [
+		// Targets are the basic building blocks of a package, defining a module or a test suite.
+		// Targets can depend on other targets in this package and products from dependencies.
+		.executableTarget(
+			name:"wg-test-tool",
+			dependencies:[
+				"wireguard-userspace-nio",
+				.product(name:"ArgumentParser", package:"swift-argument-parser")
+			]
+		),
+		.target(
+			name: "wireguard-userspace-nio",
+			dependencies:[
+				.product(name:"RAW", package:"rawdog"),
+				.product(name:"RAW_dh25519", package:"rawdog"),
+				.product(name:"RAW_chachapoly", package:"rawdog"),
+				.product(name:"NIO", package:"swift-nio"),
+				.product(name:"bedrock", package:"bedrock"),
+				.product(name:"RAW_xchachapoly", package:"rawdog"),
+				.product(name:"RAW_blake2", package:"rawdog"),
+				.product(name:"RAW_hmac", package:"rawdog"),
+			]
+		),
+		.testTarget(
+			name: "wireguard-swiftTests",
+			dependencies: ["wireguard-userspace-nio"]
+		),
+	]
+)
---
+++ Sources/wg-test-tool/MainProgram.swift
@@ -0,0 +1,12 @@
+import ArgumentParser
+
+@main
+struct CLI:ParsableCommand {
+	static let configuration = CommandConfiguration(
+		commandName:"wg-test-tool",
+		abstract:"a development tool to aid in the development of the wireguard-userspace-nio target (and others).",
+		subcommands:[
+		
+		]
+	)
+}
