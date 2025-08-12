4263db7a751abe19d6518964f88c83e0562c5253
tannerdsilva <t@tannersilva.com>
2025-08-12 12:14:58 -0500

    result structs renamed

--- Sources/wireguard-crypto-core/ResultStructs/Results.swift
+++ Sources/wireguard-crypto-core/ResultStructs/Results.swift
@@ -1,31 +1,48 @@
 import RAW
 import RAW_base64
 
-@RAW_staticbuff(bytes:32)
-public struct Result32:Sendable, Hashable, Equatable, Comparable, CustomDebugStringConvertible {
-	public var debugDescription:String {
-		return "\(RAW_base64.encode(self))"
-	}
-}
+@available(*, deprecated, renamed:"Result.Bytes32")
+public typealias Result32 = Result.Bytes32
 
-@RAW_staticbuff(bytes:24)
-public struct Result24:Sendable, Hashable, Equatable, Comparable, CustomDebugStringConvertible {
-	public var debugDescription:String {
-		return "\(RAW_base64.encode(self))"
-	}
-}
+@available(*, deprecated, renamed:"Result.Bytes24")
+public typealias Result24 = Result.Bytes24
 
-@RAW_staticbuff(bytes:16)
-public struct Result16:Sendable, Hashable, Equatable, Comparable, CustomDebugStringConvertible {
-	public var debugDescription:String {
-		return "\(RAW_base64.encode(self))"
-	}
-}
+@available(*, deprecated, renamed:"Result.Bytes16")
+public typealias Result16 = Result.Bytes16
 
-@RAW_staticbuff(bytes:8)
-@RAW_staticbuff_fixedwidthinteger_type<UInt64>(bigEndian:true)
-public struct Result8:Sendable, Hashable, Equatable, Comparable, CustomDebugStringConvertible {
-	public var debugDescription:String {
-		return "\(RAW_base64.encode(self))"
+@available(*, deprecated, renamed:"Result.Bytes8")
+public typealias Result8 = Result.Bytes8
+
+/// encompasses various result types from the wireguard 
+public struct Result {}
+
+extension Result {
+	@RAW_staticbuff(bytes:8)
+	@RAW_staticbuff_fixedwidthinteger_type<UInt64>(bigEndian:true)
+	public struct Bytes8:Sendable, Hashable, Equatable, Comparable, CustomDebugStringConvertible {
+		public var debugDescription:String {
+			return "\(RAW_base64.encode(self))"
+		}
+	}
+
+	@RAW_staticbuff(bytes:16)
+	public struct Bytes16:Sendable, Hashable, Equatable, Comparable, CustomDebugStringConvertible {
+		public var debugDescription:String {
+			return "\(RAW_base64.encode(self))"
+		}
+	}
+
+	@RAW_staticbuff(bytes:24)
+	public struct Bytes24:Sendable, Hashable, Equatable, Comparable, CustomDebugStringConvertible {
+		public var debugDescription:String {
+			return "\(RAW_base64.encode(self))"
+		}
+	}
+
+	@RAW_staticbuff(bytes:32)
+	public struct Bytes32:Sendable, Hashable, Equatable, Comparable, CustomDebugStringConvertible {
+		public var debugDescription:String {
+			return "\(RAW_base64.encode(self))"
+		}
 	}
 }
