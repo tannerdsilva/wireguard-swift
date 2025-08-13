fba5b9927acb429892e32bd24c99d10209e8968c
tannerdsilva <t@tannersilva.com>
2025-08-13 17:05:10 -0500

    remove legacy Result symbold

--- Sources/wireguard-crypto-core/ResultStructs/Results.swift
+++ Sources/wireguard-crypto-core/ResultStructs/Results.swift
@@ -1,18 +1,6 @@
 import RAW
 import RAW_base64
 
-@available(*, deprecated, renamed:"Result.Bytes32")
-public typealias Result32 = Result.Bytes32
-
-@available(*, deprecated, renamed:"Result.Bytes24")
-public typealias Result24 = Result.Bytes24
-
-@available(*, deprecated, renamed:"Result.Bytes16")
-public typealias Result16 = Result.Bytes16
-
-@available(*, deprecated, renamed:"Result.Bytes8")
-public typealias Result8 = Result.Bytes8
-
 /// encompasses various result types from the wireguard 
 public struct Result {}
 
